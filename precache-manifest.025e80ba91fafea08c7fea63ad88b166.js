48a8c4f1ca8ce426e45fac2a63cc43479e3fd343
Keeveee <shypanties22@gmail.com>
2023-01-02 21:22:30 +0800

    initialize website build

---
+++ precache-manifest.025e80ba91fafea08c7fea63ad88b166.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe3b846c6547e47a6b5e0ef07ad8617d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "56e65c548035a8e16230",
+    "url": "/static/css/main.236b1b2f.chunk.css"
+  },
+  {
+    "revision": "29c82b2e8eba6de54fd5",
+    "url": "/static/js/2.967987a3.chunk.js"
+  },
+  {
+    "revision": "56e65c548035a8e16230",
+    "url": "/static/js/main.52b96526.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "eb3785eea77861e9551bbbf36822340e",
+    "url": "/static/media/trollface.eb3785ee.png"
+  }
+]);
